ba635412c7c714a757dca6879f1f4ff38e949e25
dglav <glavich_drew@yahoo.com>
2019-09-28 23:30:17 +0900

    Added signin page, its component, and custom form-input and custom-button components to be reused in other parts of the app.

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up-page.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./sign-in-and-sign-up-page.styles.scss";
+
+import SignIn from "../../components/sign-in/sign-in.component";
+
+const SignInAndSignUpPage = () => (
+  <div>
+    <div className="sign-in-and-sign-up">SIGN IN</div>
+    <SignIn />
+  </div>
+);
+
+export default SignInAndSignUpPage;
