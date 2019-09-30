e5e56c80c90bcb156eb537b19039a9fb1783e4d1
dglav <glavich_drew@yahoo.com>
2019-09-30 23:37:55 +0900

    Implemented firebase utils to create a userProfileDoc, included ability to store authenticated user in database, and added sign-up component to sign-in-and-sign-up page

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,30 @@ const config = {
   measurementId: "G-1PBCYDT8E8"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = Date();
+
+    try {
+      userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (err) {
+      console.log("Error creating user", err.message);
+    }
+  }
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up-page.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up-page.component.jsx
@@ -3,11 +3,14 @@ import React from "react";
 import "./sign-in-and-sign-up-page.styles.scss";
 
 import SignIn from "../../components/sign-in/sign-in.component";
+import SignUp from "../../components/sign-up/sign-up.component";
 
 const SignInAndSignUpPage = () => (
   <div>
-    <div className="sign-in-and-sign-up">SIGN IN</div>
-    <SignIn />
+    <div className="sign-in-and-sign-up">
+      <SignIn />
+      <SignUp />
+    </div>
   </div>
 );
 
