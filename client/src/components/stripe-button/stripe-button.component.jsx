58adeeb4c18f635c4a39b6eb82dc8941d72676cf
dglav <glavich_drew@yahoo.com>
2019-10-20 15:14:13 +0900

    Added client-side support to interface with Stripe backend payments

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = "pk_test_8DrWhDZgf6FGQ6bJacXzfZx100rljaAEd7";
 
   const onToken = token => {
-    console.log(token);
-    alert("Payment Successful");
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    })
+      .then(response => {
+        alert("Payment Successful");
+      })
+      .catch(error => {
+        console.log("Payment error: ", JSON.parse(error));
+        alert(
+          "There was a problem with your payment. Please make sure you use the provided credit card."
+        );
+      });
   };
 
   return (
